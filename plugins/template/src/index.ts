59c07b110eca1d1fc83a2f34239de9e6b4017eab
Dreamhacker-Fear <lucaebraeuer@gmail.com>
2026-08-21 12:18:53 +0200

    Implement hide/unhide channel functionality

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,12 +1,47 @@
+import { metro } from "@vendetta";
+import { storage } from "@vendetta/plugin";
 import { logger } from "@vendetta";
+
 import Settings from "./Settings";
 
-export default {
-    onLoad: () => {
-        logger.log("Hello world!");
-    },
-    onUnload: () => {
-        logger.log("Goodbye, world.");
-    },
-    settings: Settings,
+const hiddenChannels: string[] = storage.hiddenChannels ?? [];
+
+function save() {
+    storage.hiddenChannels = hiddenChannels;
 }
+
+function isHidden(id: string) {
+    return hiddenChannels.includes(id);
+}
+
+function hideChannel(id: string) {
+    if (!isHidden(id)) {
+        hiddenChannels.push(id);
+        save();
+    }
+}
+
+function unhideChannel(id: string) {
+    const index = hiddenChannels.indexOf(id);
+
+    if (index !== -1) {
+        hiddenChannels.splice(index, 1);
+        save();
+    }
+}
+
+export default {
+    onLoad() {
+        logger.log("Hide Channels loaded");
+    },
+
+    onUnload() {
+        logger.log("Hide Channels unloaded");
+    },
+
+    settings: Settings,
+
+    hideChannel,
+    unhideChannel,
+    isHidden,
+};
