2667199f816bc025f239b334fa955caea2bc7fd3
Dreamhacker-Fear <lucaebraeuer@gmail.com>
2026-08-21 12:19:21 +0200

    Update Settings component to show hidden channels
    Refactor Settings component to display hidden channels count and IDs.

--- plugins/template/src/Settings.tsx
+++ plugins/template/src/Settings.tsx
@@ -1,8 +1,26 @@
 import { Forms } from "@vendetta/ui/components";
-const { FormText } = Forms;
+import { storage } from "@vendetta/plugin";
+import { React } from "@vendetta/metro/common";
 
-export default () => (
-    <FormText>
-        Hello, world!
-    </FormText>
-)
+const { FormText, FormSection } = Forms;
+
+export default () => {
+    const [, refresh] = React.useState(0);
+    const hidden: string[] = storage.hiddenChannels ?? [];
+
+    return (
+        <FormSection title="Hide Channels">
+            <FormText>
+                Hidden channels: {hidden.length}
+            </FormText>
+
+            <FormText>
+                Channel IDs:
+                {"\n\n"}
+                {hidden.length
+                    ? hidden.join("\n")
+                    : "No channels hidden."}
+            </FormText>
+        </FormSection>
+    );
+};
